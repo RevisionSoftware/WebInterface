a8b548fe8d0d5c85d462f78e14580144e9a871f9
rmd1995 <delga051@cougars.csusm.edu>
2017-04-09 15:03:55 -0700

    html pages for tables delete and add

---
+++ html/DeleteDataPickup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--Item ID search Form  -->
+<html lang = "en">
+   <head>
+<title> PHP Script for Bins Table </title>
+<link rel="stylesheet" type="text/css" href="../css/HomePage.css">
+<link rel="stylesheet" type="text/css" href="../css/AdminPage.css">
+<link rel="stylesheet" type="text/css" href="../css/Modal.css">
+   </head>
+<body>
+<?php
+			     // Create connection
+$connection = include '../php/ConnectDB.php';
+
+// Get the Pickup_ID entered by the user in the ID box
+$PID = $_POST['PID'];
+$PID = stripcslashes($PID);
+$PIDU = htmlspecialchars($PID);
+//query to select info from Items
+$query = "DELETE FROM Pickup WHERE Pickup_ID = '$PIDU'";
+
+//Variable to store query result
+$Qresult = mysqli_query($db, $query);
+
+if(!$Qresult)//if statement for checking query
+  {
+    print "Error - The query was not successful.";
+    $error = mysql_error();//sql error
+    print "<p>" . $error . "</p>";
+    exit;
+  }
+ else{print "PICKUP DELETED";}
+?>
+</body>
+</html>
