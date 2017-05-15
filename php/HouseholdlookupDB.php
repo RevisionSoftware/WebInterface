2b236434f41f1ee8f6cdcc3b5c8f18bce302185b
rmd1995 <delga051@cougars.csusm.edu>
2017-05-14 20:07:55 -0700

    new updated on household data

--- php/HouseholdlookupDB.php
+++ php/HouseholdlookupDB.php
@@ -107,6 +107,129 @@ if($n_rows > 0)
 // Close table
 print "</table>";
 print "</div>";
+$query = "SELECT * FROM Bins WHERE Account_Num = '$HIDU'";
+
+//Variable to store query result
+$Qresult = mysqli_query($connection, $query);
+
+if(!$Qresult)//if statement for checking query
+  {
+    print "Error - The query was not successful.";
+    $error = mysql_error();//sql error
+    print "<p>" . $error . "</p>";
+    exit;
+  }
+
+// Display the results in a table
+print "<div>";
+print "<table> <caption> <h1> Bins Table: </h1>";
+print "<tr align = 'center'>";
+
+
+$n_rows = mysqli_num_rows($Qresult);// Number of rows var
+
+if($n_rows > 0)
+  {
+    // Get the first row of the result
+    $firstRow = mysqli_fetch_assoc($Qresult);
+
+    // Get the number of fields in the result
+    $num_fields = mysqli_num_fields($Qresult);
+
+    // Get the column names
+    $CNames = array_keys($firstRow);
+
+    // Display column names as appropriate column headers
+    for($i = 0; $i < $num_fields; $i++)
+      {
+	print "<th>" . $CNames[$i] . "</th>";
+      }
+    print "</tr>";
+
+    // Display the values of rows
+    for($row_num = 0; $row_num < $n_rows; $row_num++)
+      {
+	print "<tr>";
+	$values = array_values($firstRow);
+	for($i = 0; $i < $num_fields; $i++)
+	  {
+	    $value = htmlspecialchars($values[$i]);
+	    print "<td>" . $value . "</td>";
+	  }
+	print "</tr>";// End row
+	$firstRow = mysqli_fetch_assoc($Qresult);//next row
+      }
+  }
+ else//else statement if nothing is inputed or false
+   {
+     print "<td> *****NO TABLE DATA*****.</td> <br />";
+   }
+
+// Close table
+print "</table>";
+print "</div>";
+
+$query = "SELECT * FROM Pickup WHERE Account_Num = '$HIDU'";
+
+//Variable to store query result
+$Qresult = mysqli_query($connection, $query);
+
+if(!$Qresult)//if statement for checking query
+  {
+    print "Error - The query was not successful.";
+    $error = mysql_error();//sql error
+    print "<p>" . $error . "</p>";
+    exit;
+  }
+
+// Display the results in a table
+print "<div>";
+print "<table> <caption> <h1> Pickup Table: </h1>";
+print "<tr align = 'center'>";
+
+
+$n_rows = mysqli_num_rows($Qresult);// Number of rows var
+
+if($n_rows > 0)
+  {
+    // Get the first row of the result
+    $firstRow = mysqli_fetch_assoc($Qresult);
+
+    // Get the number of fields in the result
+    $num_fields = mysqli_num_fields($Qresult);
+
+    // Get the column names
+    $CNames = array_keys($firstRow);
+
+    // Display column names as appropriate column headers
+    for($i = 0; $i < $num_fields; $i++)
+      {
+	print "<th>" . $CNames[$i] . "</th>";
+      }
+    print "</tr>";
+
+    // Display the values of rows
+    for($row_num = 0; $row_num < $n_rows; $row_num++)
+      {
+	print "<tr>";
+	$values = array_values($firstRow);
+	for($i = 0; $i < $num_fields; $i++)
+	  {
+	    $value = htmlspecialchars($values[$i]);
+	    print "<td>" . $value . "</td>";
+	  }
+	print "</tr>";// End row
+	$firstRow = mysqli_fetch_assoc($Qresult);//next row
+      }
+  }
+ else//else statement if nothing is inputed or false
+   {
+     print "<td> *****NO TABLE DATA*****.</td> <br />";
+   }
+
+// Close table
+print "</table>";
+print "</div>";
 ?>
 </main>
 </body>
